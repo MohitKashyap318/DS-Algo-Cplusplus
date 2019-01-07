fc5c9aa962d6b9a97f8e7948cfa42332d43b32b8
mohit kashyap <kashyapmohit318@gmail.com>
2019-01-07 22:40:12 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/DS_Algo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DS_Algo.dir/helloworld/helloworld.cpp.obj"
+  "DS_Algo.pdb"
+  "DS_Algo.exe"
+  "DS_Algo.exe.manifest"
+  "libDS_Algo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DS_Algo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,11 @@
+//
+// Created by USER on 1/7/2019.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World from my c++ ide Clion " <<endl;
+    return 0;
+}
